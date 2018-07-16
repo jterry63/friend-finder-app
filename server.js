c16f92ed57874d8a13faf1f5a864a9ed97f08972
jterry63 <jordankterry@outlook.com>
2018-07-16 09:30:53 -0600

    first-commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var http = require("http");
+var fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+// var PORT = process.env.PORT || 3000;
+var PORT = 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+require("./routing/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
